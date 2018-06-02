d5d59ed9d694e6244e9a5fd1e1fc0b58e1f05e97
Gabriel Alvarado <Gabriel_Alvarado24@hotmail.com>
2018-06-01 18:07:55 -0700

    Agregar ingredientes

--- main.cpp
+++ main.cpp
@@ -14,37 +14,42 @@ using std::cin;
 using std::string;
 
 int main(){
-     vector<menu*>listMenu;
      vector<bodega*>listBodega;
-     	bodega* b = new bodega();     
+     vector<ingredientes*>ListIngre;
+     	bodega* b = new bodega();   
 		cout<<"Bienvenido" <<endl;  
 		int opcion=0;
 		while(opcion!=5){
 		
-		cout<<"1) ingresar ingredientes de bodega"<<endl;
-		cout<<"2) Menu"<<endl;
+		cout<<"1) ingresar ingredientes"<<endl;
+		cout<<"2) Bodega"<<endl;
 		cout<<"3) Crear Platos"<<endl;
 		cout<<"4) experiencia"<<endl;
 		cout<<"5) SAlIR"<<endl;
+		cout<<"Ingrese una opcion"<<endl;	
 		cin>>opcion;	
 		if(opcion==1){
 		char resp='s';
 		while(resp=='s'||resp=='S'){
-		string ingredientes;
-		int cantidad;
-		cout<<"Ingrese el nombre del ingrediente:"<<endl;
-		cin>>ingredientes;
-		cout<<"Ingrese la cantidad:"<<endl;
-		cin>>cantidad;
-		//b = new bodega(cantidad,ingredientes);
-		
-		
+			string nombre;
+			string tipo;
+			cout<<"Ingrese el nombre del ingrediente:"<<endl;
+			cin>>nombr:we;
+			cout<<"Ingrese la cantidad:"<<endl;
+			cin>>tipo;
+			b->setIngredientes(new ingredientes(nombre,tipo));	
 		
 		cout<<"Desea ingresar otro ingrediente s/n"<<endl;
 		cin>>resp;
 		}//		
 		
+		}
 		
+		if(opcion==2){
+			int cantidad;
+		cout<<"Ingrese la cantidad de ingredientes:"<<endl;		
+		cin>>cantidad;
+				
 		}
 		
 			
